88f3e6f3d977e1dbd305988fc99f98a1e976c431
tuagkcuf <jokersms222@gmail.com>
2023-04-16 18:58:54 +0300

    add tests

---
+++ contracts/Dappcord.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Dappcord is ERC721 {
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        
+    }
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,4 +5,26 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), "ether")
 }
 
-describe("Dappcord", function () {})
+describe("Dappcord", function () {
+    let dappcord
+
+    const NAME = "Dappcord"
+    const SYMBOL = "DC"
+
+    beforeEach(async () => {
+        const Dappcord = await ethers.getContractFactory("Dappcord")
+        dappcord = await Dappcord.deploy(NAME, SYMBOL)
+    })
+
+    describe("Deployment", function () {
+        it("Sets the name", async () => {
+            let result = await dappcord.name()
+            expect(result).to.equal(NAME)
+        })
+
+        it("Sets the symbol", async () => {
+            let result = await dappcord.symbol()
+            expect(result).to.equal(SYMBOL)
+        })
+    })
+})
