45474ba96cf52fe2a4a39c3e3e1b5a300bf9ef78
tuagkcuf <jokersms222@gmail.com>
2023-04-16 19:39:14 +0300

    add channels

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
+    uint256 public totalChannels;
     address public owner;
+
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping (uint256 => Channel) channels;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function createChannel(string memory _name, uint256 _cost) public {
+        totalChannels++;
+        channels[totalChannels] = Channel(totalChannels, _name, _cost);
     }
 }
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -13,10 +13,15 @@ describe("Dappcord", function () {
     const SYMBOL = "DC"
 
     beforeEach(async () => {
+        // Setup accounts
         [deployer, user] = await ethers.getSigners()
 
+        // Deploy contract
         const Dappcord = await ethers.getContractFactory("Dappcord")
         dappcord = await Dappcord.deploy(NAME, SYMBOL)
+
+        // Create a channel
+        const transaction = await dappcord.createChannel("general", tokens(1))
     })
 
     describe("Deployment", function () {
@@ -33,6 +38,13 @@ describe("Dappcord", function () {
         it("Sets the owner", async () => {
             const result = await dappcord.owner()
             expect(result).to.equal(deployer.address)
+        })
+    })
+
+    describe("Creating Channels", function () {
+        it("Returns total channels", async () => {
+            const result = (await dappcord.totalChannels()).toString()
+            expect(result).to.be.equal("1")
         })
     })
 })
