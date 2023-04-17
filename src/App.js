3c074e4c7f8925ae52919887b538d11b10ccbe5d
tuagkcuf <jokersms222@gmail.com>
2023-04-17 18:04:41 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
 import { io } from "socket.io-client"
 
 // Components
-import Navigation from './components/Navigation'
-import Servers from './components/Servers'
-import Channels from './components/Channels'
-import Messages from './components/Messages'
+import Navigation from "./components/Navigation"
+import Servers from "./components/Servers"
+import Channels from "./components/Channels"
+import Messages from "./components/Messages"
 
 // ABIs
-import Dappcord from './abis/Dappcord.json'
+import Dappcord from "./abis/Dappcord.json"
 
 // Config
-import config from './config.json';
+import config from "./config.json"
 
 // Socket
-const socket = io('ws://localhost:3030');
+const socket = io("ws://localhost:3030")
 
 function App() {
+    const loadBlockchaindData = async () => {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+        const account = ethers.utils.getAddress(accounts[0])
+        console.log(account)
+    }
 
-  return (
-    <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
+    useEffect(() => {
+        loadBlockchaindData()
+    }, [])
 
-      <main>
-
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Navigation />
+            <main></main>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers"
 
 const Navigation = ({ account, setAccount }) => {
+    return (
+        <nav>
+            <div className="nav__brand">
+                <h1>Dappcord</h1>
+            </div>
 
-  return (
-    <nav>
-
-    </nav>
-  );
+            <button type="button" className="nav__connect">
+                Connect
+            </button>
+        </nav>
+    )
 }
 
-export default Navigation;
+export default Navigation
